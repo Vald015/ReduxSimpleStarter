930183c6f423b6bf2457bb79faac35a0fdbe3380
Osvaldo <osvaldo.ramirez@upr.edu>
2017-11-28 17:40:54 -0400

    Search bar

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react'; //explicitly declare the use of react
-import ReactDOM from 'react-DOM';
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyBqSXPRmhiju0U-T2EP6fVONQp3j-yAGqw';
 
 // Create a new component. This componentshould produce
 // some HTML
 
-const App = function(){
-  return <div>Hi!</div>;
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Take this component's generated HTML and put it
 // on the page (in the DOM)
-React.render(App);
+ReactDOM.render(<App />,document.querySelector('.container'));
