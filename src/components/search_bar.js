f59eec56bd30689d5b341eed0c2124db3d49929e
Osvaldo <osvaldo.ramirez@upr.edu>
2017-11-28 18:05:16 -0400

    SearchBar w/ console log

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const SearchBar = () => {
-  return <input />;
-};
+/* Search bar class */
+class SearchBar extends Component {
+  /* Render function */
+  render(){
+      return <input onChange = {event => console.log(event.target.value)} />; /* Return JSX component */
+  }
+
+  //onInputChange(event) {
+  //    console.log(event.target.value);
+  //}
+}
 
 export default SearchBar;
