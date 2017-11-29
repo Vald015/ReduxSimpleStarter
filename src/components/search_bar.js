a990e5046e7092332346d7c7c6b809319f3a2c5c
Osvaldo <osvaldo.ramirez@upr.edu>
2017-11-28 22:58:16 -0400

    styling & stuff Lecture 2 done

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -12,7 +12,7 @@ class SearchBar extends Component {
   /* Render function to return JSX component */
   render(){
       return(
-        <div>
+        <div className = "search-bar">
           <input
             value = {this.state.term}
             onChange = {event => this.setState({term: event.target.value})} />
